e0f2071b321c1d2d1b1f6eea49a3107d2eeef178
Edouard Biton <aarklendoia@outlook.com>
2019-12-17 15:37:18 +0100

    Add new API /setstate for camera.

--- Aark.Netatmo.SDK/Models/APICommands.cs
+++ Aark.Netatmo.SDK/Models/APICommands.cs
@@ -501,6 +501,34 @@ namespace Aark.Netatmo.SDK.Models
             }
         }
 
+        internal async Task<SimpleAnswer> SetStatus()
+        {
+            if (!await CheckConnectionAsync().ConfigureAwait(false))
+                return null;
+            var parameters = HttpUtility.ParseQueryString(string.Empty);
+
+            using (HttpClient client = new HttpClient())
+            using (HttpRequestMessage request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(host + apiPath + "/setstate");
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+                request.Content = new StringContent(parameters.ToString(), Encoding.UTF8, "application/x-www-form-urlencoded");
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                string responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                SimpleAnswer simpleAnswer = new SimpleAnswer().FromJson(responseBody);
+                if (simpleAnswer.Status == null)
+                {
+                    ErrorMessageData errorMessageData = new ErrorMessageData().FromJson(responseBody);
+                    _errorMessage = errorMessageData.Error.Message;
+                    return null;
+                }
+                return simpleAnswer;
+            }
+        }
+
+
         internal async Task<SimpleAnswer> RegisterWebHook(Uri url)
         {
             if (!await CheckConnectionAsync().ConfigureAwait(false))
